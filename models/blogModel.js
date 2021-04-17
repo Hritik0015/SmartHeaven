18e90133b9f5b61acc28da5d47859e60d47bc2c3
rawalprakash722 <rawalprakash722@gmail.com>
2021-04-17 08:08:03 +0545

    blog models

---
+++ models/blogModel.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose')
+const router=require('../route/blog_route')
+
+
+const blog=mongoose.model('Blog',{
+    title:{
+        type:String,
+        require:true
+    },
+
+    message:{
+        type:String,
+        require:true
+        
+    },
+
+
+
+
+})
+
+module.exports=blog;
