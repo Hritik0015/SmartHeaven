8a33e4128be9c7e68363de77a626af956d1ae7ef
rawalprakash722 <rawalprakash722@gmail.com>
2021-04-17 07:59:05 +0545

    blog route app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db=require('./database/db');
 const register_route=require('./route/register_route');
 const product_route=require('./route/product_route')
 const contact_route=require('./route/contact_route')
-
+const blog_route = require('./route/blog_route')
 const path=require('path');
 
 
@@ -21,5 +21,5 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(register_route)
 app.use(product_route)
 app.use(contact_route)
-
+app.use(blog_route)
 app.listen(90)
